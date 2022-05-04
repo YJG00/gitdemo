5f48ce7909b8dc763248990b08159af3ff261afd
yjg <2249119220@qq.com>
2022-05-04 20:07:21 +0800

    first commit

---
+++ src/main/java/com/yjg/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.yjg.git;
+
+/**
+ * @author 陌格
+ * @version 1.0
+ * @date 2022/5/4 19:25
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
