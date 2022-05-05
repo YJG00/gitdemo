c62292d82896f0b68e11cfc5f8c6f030fe4e86e0
yjg <2249119220@qq.com>
2022-05-05 19:50:11 +0800

    push commit

--- src/main/java/com/yjg/git/GitTest.java
+++ src/main/java/com/yjg/git/GitTest.java
@@ -8,5 +8,10 @@ package com.yjg.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git111");
+        System.out.println("hello git222");
+        System.out.println("hello git333");
+        System.out.println("hello git444");
+        System.out.println("hello git555");
     }
 }
